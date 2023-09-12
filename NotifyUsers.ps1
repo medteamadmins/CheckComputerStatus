c2a9776868a15f925a256831c787714455336670
medteamadmins <143048010+medteamadmins@users.noreply.github.com>
2023-09-12 15:06:36 -0400

    Add files via upload
    This script checks the computer status, If unexpected users have been removed from the Local Admin Group a pop-up to notify users that the computer is ready. Otherwise shows the computer is setting up.

---
+++ NotifyUsers.ps1
@@ -0,0 +1,25 @@
+# Define the list of expected members in the Administrators group
+$expectedMembers = @("Administrator", "MiPCSAdmin")
+
+# Get the members of the Administrators group
+$administratorsGroup = [ADSI]"WinNT://./Administrators,group"
+$members = $administratorsGroup.Invoke("Members") | ForEach-Object {
+    $_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)
+}
+
+# Check if the members match the expected list
+$additionalMembers = $members | Where-Object { $_ -notin $expectedMembers }
+
+# Display the appropriate notification
+if ($additionalMembers.Count -eq 0) {
+    # There are no additional members
+    [System.Windows.Forms.MessageBox]::Show("The computer is ready to use", "Computer Status", "OK", [System.Windows.Forms.MessageBoxIcon]::Information)
+} else {
+    # There are additional members
+    [System.Windows.Forms.MessageBox]::Show("Please DO NOT USE this computer at this moment.
+
+We are setting up this computer. It will restart as we complete", "Configuration is in Progress", "OK", [System.Windows.Forms.MessageBoxIcon]::Stop)
+
+# Restart computer in 40 minutes
+shutdown.exe /r -t 2400
+}
